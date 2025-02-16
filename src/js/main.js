de82a42b65811bcb8c1d9329b9d3d400d54ac0a7
elisha111 <elisey.kovalyov@yandex.ru>
2025-02-16 21:34:08 +0300

    начало проета, тестировал переменные scss

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,4 @@
-// import '../scss/style.scss'
-
-// ИМПОРТ ШАПКИ И ПОДВАЛА
-async function loadComponent(id, url) {
-    const container = document.getElementById(id);
-    if (container) {
-        const response = await fetch(url);
-        container.innerHTML = await response.text();
-    }
-}
-
-loadComponent("header", "/src/templates/header.html");
-loadComponent("footer", "/src/templates/footer.html");
-
-
-// ВСПЛЫВАШКА 
-const modal = document.querySelector('#modal');
-const btn = document.querySelector('#openModal');
-const close = document.querySelector('.close');
-
-btn.onclick = function () {
-  modal.style.display = 'block';
-};
-
-close.onclick = function () {
-  modal.style.display = 'none';
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-};
+// КУРСОР 
+document.addEventListener('mousemove', e => {
+  document.body.style.cssText = `--move-x: ${e.clientX}px; --move-y: ${e.clientY}px;`
+})
